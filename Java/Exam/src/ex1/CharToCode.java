3b99ea74673fb9830b02cc0c96c646bcd48c40c0
GoldenMoonByr <milksat11@gmail.com>
2020-04-09 19:47:04 +0900

    Update CharToCode.java

--- Java/Exam/src/ex1/CharToCode.java
+++ Java/Exam/src/ex1/CharToCode.java
@@ -7,7 +7,7 @@ public class CharToCode {
 		char c = 'B'; // char ch = '\u0042'; 유니코드의 16진수 화
 
 		
-	int code = (int)c; // 문자값을 int형으로 변환한다.
+	int code = (int)c; // 문자값을 int형으로 변환한다. , (int) 생락가능.
 		
 		System.out.println(c);
 
