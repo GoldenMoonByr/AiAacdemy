8abf61da2971b87f5b0e9fe1b6c99384984d036f
GoldenMoonByr <milksat11@gmail.com>
2020-04-14 18:22:25 +0900

    2020-04-14
    2020-04-14

--- Java/Fruit/src/FruitMain.java
+++ Java/Fruit/src/FruitMain.java
@@ -4,14 +4,14 @@ public class FruitMain {
 		FruitBuyer buyer = new FruitBuyer();
 		FruitSeller seller = new FruitSeller();
 		buyer.appleBuyingSystem(5000, seller);
-		
+
 		System.out.println("판매자 판매현황판");
-		System.out.println("수입 : "+seller.income);
-		System.out.println("남은 사과 개수"+seller.numOfApple);
+		System.out.println("수입 : " + seller.income);
+		System.out.println("남은 사과 개수" + seller.numOfApple);
 		System.out.println("-----------------------------------");
 		System.out.println("구입자 구매현황판");
-		System.out.println("구입한 사과 개수"+buyer.myApple);
-		System.out.println("남은 돈 :" +buyer.myMoney);
+		System.out.println("구입한 사과 개수" + buyer.myApple);
+		System.out.println("남은 돈 :" + buyer.myMoney);
 	}
 
 }
--- Java/Fruit/src/FruitSeller.java
+++ Java/Fruit/src/FruitSeller.java
@@ -16,5 +16,4 @@ public class FruitSeller {
 		income = income + money;
 		return num;
 	}
-
 }
