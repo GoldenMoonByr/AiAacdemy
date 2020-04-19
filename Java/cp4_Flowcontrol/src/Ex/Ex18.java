1e28ceb9bdedf4ea8a12ef0f942e3cd33a373498
GoldenMoonByr <63330372+GoldenMoonByr@users.noreply.github.com>
2020-04-19 16:43:04 +0900

    Update Ex18.java

--- Java/cp4_Flowcontrol/src/Ex/Ex18.java
+++ Java/cp4_Flowcontrol/src/Ex/Ex18.java
@@ -1,11 +1,15 @@
 package Ex;
+
 // 1+(1+2)+(1+2+3)+(1+2+3+4)+...+(1+2+3+...+10)의 결과를 계산하시오.
 //미완
 public class Ex18 {
 	public static void main(String[] args) {
 		int a=0, sum=0;
 				for(int i=1;i<10;i++) {
-	
-	
+					sum=sum+i;
+					a =sum+a;
+					System.out.println(sum+","+a);
+				}
 	}
-
+	
+}
